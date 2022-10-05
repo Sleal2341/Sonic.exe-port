bca02454efa6e30ca61c5089a6869b0f20d6abb6
Sleal2341 <102236262+Sleal2341@users.noreply.github.com>
2022-10-05 13:41:02 -0500

    Add files via upload

---
+++ assets/preload/data/you-cant-run-encore/script.lua
@@ -0,0 +1,10 @@
+function onCreate()
+	precacheImage('characters/bfbunnypixel')
+	precacheImage('characters/sonicpixel')
+	precacheImage('ycr/greenbg')
+	precacheImage('ycr/bg')
+	precacheImage('ycr/fire')
+	precacheImage('characters/encorebf')
+	precacheImage('characters/encoregf')
+	precacheImage('characters/YCR-Sonic-Angry')
+end
